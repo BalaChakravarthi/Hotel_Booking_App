60510b61eb57b4a38d4f41b5c061bc3cba1d8c01
BalaChakravarthi <balachakravarthidhupati@gmail.com>
2026-03-05 17:25:02 +0530

    add frontend domain

--- Backend/hotel_backend/hotel_backend/settings.py
+++ Backend/hotel_backend/hotel_backend/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://hotelbookingapp-mu.vercel.app",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'hotel_backend.urls'
